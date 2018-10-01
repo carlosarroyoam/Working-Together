a29e4dc7fb46092b5b514c6ff820fe9f6db3eadf
Carlos Alberto Arroyo Martinez <43684710+carlosarroyoam@users.noreply.github.com>
2018-09-30 22:55:05 -0500

    Se agrego menu lateral menu notificaciones

---
+++ app/src/main/java/com/workingtogether/workingtogether/adapter/SignupSlideAdapter.java
@@ -0,0 +1,62 @@
+package com.workingtogether.workingtogether.adapter;
+
+import android.content.Context;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+
+import com.workingtogether.workingtogether.R;
+
+public class SignupSlideAdapter extends PagerAdapter {
+    private Context context;
+    private LayoutInflater layoutInflater;
+    private int[] slide_layouts = {
+            R.layout.activity_signup_step_one,
+            R.layout.activity_signup_step_two,
+            R.layout.activity_signup_step_three
+    };
+
+    public SignupSlideAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return slide_layouts.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.activity_signup_slide, container, false);
+
+        ViewStub stub = view.findViewById(R.id.layout_loader);
+        stub.setLayoutResource(slide_layouts[position]);
+        View inflated = stub.inflate();
+
+        //Si el View tiene elementos hijos primero eliminalos
+        if (inflated.getParent() != null)
+            ((ViewGroup) inflated.getParent()).removeView(inflated);
+
+        container.addView(inflated);
+
+        return inflated;
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+    }
+}
